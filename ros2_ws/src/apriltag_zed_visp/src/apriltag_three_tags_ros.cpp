8ea71cfbe7d1da0271e5204ab2470164f30f0c17
chisetaiyan <1234556@qq.com>
2026-07-13 10:16:45 +0800

    Fix IMU display (was not inserted)

--- ros2_ws/src/apriltag_zed_visp/src/apriltag_three_tags_ros.cpp
+++ ros2_ws/src/apriltag_zed_visp/src/apriltag_three_tags_ros.cpp
@@ -1382,6 +1382,11 @@ int main(int argc, char** argv) {
             cv::putText(frame_left, in_roi?"VALID (20-80cm)":"OUT OF RANGE", Point(rx+5,ry+20), FONT_HERSHEY_SIMPLEX, 0.7, in_roi?Scalar(0,255,0):Scalar(0,0,255), 2);
             if(!g_bref_ok) putText(frame_left,"Press B to set reference",Point(img_w/2-110,img_h-30),FONT_HERSHEY_SIMPLEX,0.6,Scalar(0,255,255),2);
             else { double mv=0,dd=0; if(id1_found){ Mat R0b=rvecToMatrix(id0_rvec); Mat t1b=R0b.t()*(Mat(id1_tvec)-Mat(id0_tvec)); double d01=sqrt(t1b.at<double>(0)*t1b.at<double>(0)+t1b.at<double>(1)*t1b.at<double>(1)+t1b.at<double>(2)*t1b.at<double>(2)); mv=fabs((d01-g_bref_d01)*1000); } dd=fabs((g_cap_d-g_bref_d02)*1000); stringstream sb; sb<<fixed<<setprecision(1)<<"dCAM:"<<mv<<"mm dID2:"<<dd<<"mm"; Scalar cl=id1_found?(mv<2?Scalar(0,255,0):Scalar(0,0,255)):Scalar(0,200,200); putText(frame_left,sb.str(),Point(img_w-230,50),FONT_HERSHEY_SIMPLEX,0.45,cl,2); }
+            zed.getSensorsData(imu_data, sl::TIME_REFERENCE::CURRENT);
+            auto a=imu_data.imu.linear_acceleration; auto g=imu_data.imu.angular_velocity;
+            double acc=sqrt(a.x*a.x+a.y*a.y+a.z*a.z), gyr=sqrt(g.x*g.x+g.y*g.y+g.z*g.z);
+            stringstream simu; simu<<fixed<<setprecision(3)<<"IMU a:"<<acc<<" g:"<<gyr;
+            putText(frame_left,simu.str(),Point(20,img_h-20),FONT_HERSHEY_SIMPLEX,0.4,Scalar(0,255,0),1);
             imshow("三标签基准系统 (ID0+ID1 -> ID2)", frame_left);
 
             // 按 ESC 键退出
