be6119a34219da4fb6ff645b35d9dc9491232fd2
chisetaiyan <1234556@qq.com>
2026-07-14 12:23:59 +0800

    ULTRA depth mode + depth Z display for each tag

--- ros2_ws/src/apriltag_zed_visp/src/apriltag_three_tags_ros.cpp
+++ ros2_ws/src/apriltag_zed_visp/src/apriltag_three_tags_ros.cpp
@@ -56,6 +56,7 @@ bool g_ref_ok=false; Vec3d g_ref_t(0,0,0); Mat g_ref_R=Mat::eye(3,3,CV_64F);
 bool g_cap_ready=false;
 double g_dbg_pnpz0 = 0, g_dbg_zedz0 = -1, g_dbg_pnpz2 = 0, g_dbg_zedz2 = -1, g_dbg_zedz1 = -1;
 double g_px0=0, g_px1=0, g_px2=0;
+double g_dz0=-1, g_dz1=-1, g_dz2=-1;
 int g_dbg_frame = 0;
 
 // 标定畸变系数全局变量
@@ -669,7 +670,7 @@ int main(int argc, char** argv) {
     sl::Camera zed;
     sl::InitParameters init_params;
     init_params.camera_resolution = sl::RESOLUTION::HD720;  // 分辨率：1280x720
-    init_params.depth_mode = sl::DEPTH_MODE::PERFORMANCE;   // 深度模式：性能优先
+    init_params.depth_mode = sl::DEPTH_MODE::ULTRA;   // 深度模式：最高精度
     init_params.camera_fps = 30;                             // 帧率：30fps
     init_params.sensors_required = true;
 
@@ -989,6 +990,10 @@ int main(int argc, char** argv) {
                     else if (ids[i]==TARGET_TAG_ID) { g_dbg_zedz2=rd; id2_rvec = rv; id2_tvec = tv; }
                     double sl = (norm(corners[i][0]-corners[i][1])+norm(corners[i][1]-corners[i][2])+norm(corners[i][2]-corners[i][3])+norm(corners[i][3]-corners[i][0]))/4.0;
                     if(ids[i]==BASE_TAG_ID_0) g_px0=sl; else if(ids[i]==BASE_TAG_ID_1) g_px1=sl; else g_px2=sl;
+                    // Depth Z at tag center
+                    float dz=-1; int px=(int)ctr.x, py=(int)ctr.y;
+                    if(px>=0&&px<depth_undist.cols&&py>=0&&py<depth_undist.rows) dz=depth_undist.at<float>(py,px);
+                    if(ids[i]==BASE_TAG_ID_0)g_dz0=dz; else if(ids[i]==BASE_TAG_ID_1)g_dz1=dz; else g_dz2=dz;
                         double ang = acos(fabs(R_tag.at<double>(2,2)))*180/M_PI;
                         if(ids[i]==BASE_TAG_ID_0) g_dbg_zedz0=ang;
                         else if(ids[i]==BASE_TAG_ID_1) g_dbg_zedz1=ang;
@@ -1401,7 +1406,7 @@ int main(int argc, char** argv) {
                     <<"mm dR="<<dr[0]*180/M_PI<<" "<<dr[1]*180/M_PI<<" "<<dr[2]*180/M_PI<<"deg";
                 putText(frame_left, sd.str(), Point(20, ly+5), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255,255,0), 2);
             }
-            stringstream spx; spx<<fixed<<setprecision(0)<<"Size:ID0="<<g_px0<<" ID1="<<g_px1<<" ID2="<<g_px2<<"px";
+            stringstream spx; spx<<fixed<<setprecision(0)<<"px:ID0="<<g_px0<<" ID1="<<g_px1<<" ID2="<<g_px2<<"  dZ:ID0="<<g_dz0<<" ID1="<<g_dz1<<" ID2="<<g_dz2<<"mm";
             putText(frame_left,spx.str(),Point(20, img_h/2),FONT_HERSHEY_SIMPLEX,0.45,Scalar(200,200,0),1);
             // Check if all 3 tag centers are in ROI
             bool in_roi = false;
